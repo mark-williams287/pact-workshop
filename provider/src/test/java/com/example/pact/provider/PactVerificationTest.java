096619087b7d529f6fe33571801e1b9a63ff7d17
Mark Williams <mark.williams@currencycloud.com>
2022-12-12 17:17:47 +0000

    Step 3: copy pact test to provider and run verification

---
+++ provider/src/test/java/com/example/pact/provider/PactVerificationTest.java
@@ -0,0 +1,37 @@
+package com.example.pact.provider;
+
+import au.com.dius.pact.provider.junit5.HttpTestTarget;
+import au.com.dius.pact.provider.junit5.PactVerificationContext;
+import au.com.dius.pact.provider.junit5.PactVerificationInvocationContextProvider;
+import au.com.dius.pact.provider.junitsupport.Provider;
+import au.com.dius.pact.provider.junitsupport.loader.PactFolder;
+import com.example.pact.provider.controller.ProductController;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.TestTemplate;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Provider("ProductService")
+@PactFolder("pacts")
+class PactVerificationTest {
+
+    @Autowired
+    private ProductController controller;
+
+    @LocalServerPort
+    private int port;
+
+    @BeforeEach
+    void beforeEach(PactVerificationContext context) {
+        context.setTarget(new HttpTestTarget("localhost", port));
+    }
+
+    @TestTemplate
+    @ExtendWith(PactVerificationInvocationContextProvider.class)
+    void pactVerificationTestTemplate(PactVerificationContext context) {
+        context.verifyInteraction();
+    }
+}
